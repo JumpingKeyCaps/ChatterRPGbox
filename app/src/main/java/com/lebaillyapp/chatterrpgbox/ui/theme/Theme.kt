7526ae781a8393fa95f48b47ce26031571a6fc07
Samuel Lebailly <sam.meusa@gmail.com>
2025-10-28 16:48:20 +0100

    feature(ui/theme): Tweak the App Theme  to force DarkMode + StateBar  and navbar OS color change forcing.

--- app/src/main/java/com/lebaillyapp/chatterrpgbox/ui/theme/Theme.kt
+++ app/src/main/java/com/lebaillyapp/chatterrpgbox/ui/theme/Theme.kt
@@ -17,17 +17,23 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.sp
 import com.lebaillyapp.chatterrpgbox.R
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Color(0xFF000000),
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Color(0xFF000000),
     secondary = PurpleGrey40,
     tertiary = Pink40
+
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -50,11 +56,31 @@ fun ChatterRPGboxTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicDarkColorScheme(context)
         }
 
         darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> DarkColorScheme
+    }
+
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+
+            // 1. Définir la couleur de la Barre de Statut
+            window.statusBarColor = colorScheme.primary.toArgb()
+
+            // 2. Définir la couleur de la Barre de Navigation
+            window.navigationBarColor = colorScheme.primary.toArgb()
+
+            // isAppearanceLightStatusBars = false force les icônes de la Status Bar à être CLAIRES.
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+
+            // isAppearanceLightNavigationBars = false force les icônes de la Navigation Bar à être CLAIRES.
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = false
+        }
     }
 
 
