d5506a5e33ff1509e305f5bbc64d9f9fe34aeb28
Samuel Lebailly <sam.meusa@gmail.com>
2025-10-28 15:01:27 +0100

    feature(ui/theme): Apply the custom font to the App theme.

--- app/src/main/java/com/lebaillyapp/chatterrpgbox/ui/theme/Theme.kt
+++ app/src/main/java/com/lebaillyapp/chatterrpgbox/ui/theme/Theme.kt
@@ -9,7 +9,14 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
+import com.lebaillyapp.chatterrpgbox.R
+import androidx.compose.material3.Typography
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -50,9 +57,10 @@ fun ChatterRPGboxTheme(
         else -> LightColorScheme
     }
 
+
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = pixelTypography,
         content = content
     )
 }
