b3cc64f895b5fabeed2f7b0e0448d4ce8baafca1
Samuel Lebailly <sam.meusa@gmail.com>
2025-10-30 16:58:16 +0100

    feature(ui/animation): Move all avatar speaking animation sequences in a dedicated file, for a clean and a easy way to configure.

---
+++ app/src/main/java/com/lebaillyapp/chatterrpgbox/ui/animation/SpeakingAnimations.kt
@@ -0,0 +1,59 @@
+package com.lebaillyapp.chatterrpgbox.ui.animation
+
+import com.lebaillyapp.chatterrpgbox.R
+import com.lebaillyapp.chatterrpgbox.model.AnimStep
+
+val heroAnimationSequence1 = listOf(
+    AnimStep(drawableResId = R.drawable.mrevil_angry_2, durationMs = 200L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_4, durationMs = 200L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_2, durationMs = 200L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_3, durationMs = 200L)
+)
+
+
+val heroAnimationSequence2 = listOf(
+    AnimStep(drawableResId = R.drawable.mrevil_angry_1, durationMs = 2000L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_4, durationMs = 100L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_5, durationMs = 100L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_4, durationMs = 100L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_3, durationMs = 100L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_4, durationMs = 100L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_3, durationMs = 100L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_4, durationMs = 100L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_5, durationMs = 100L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_4, durationMs = 100L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_6, durationMs = 350L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_2, durationMs = 500L)
+)
+
+
+val heroAnimationSequence5 = listOf(
+    AnimStep(drawableResId = R.drawable.mrevil_angry_1, durationMs = 1000L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_3, durationMs = 500L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_7, durationMs = 2000L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_6, durationMs = 300L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_2, durationMs = 300L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_1, durationMs = 500L),
+
+    )
+
+
+
+val heroAnimationSequence4 = listOf(
+    AnimStep(drawableResId = R.drawable.mrevil_angry_1, durationMs = 400L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_5, durationMs = 200L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_4, durationMs = 200L),
+)
+
+val heroAnimationSequence8 = listOf(
+    AnimStep(drawableResId = R.drawable.mrevil_angry_1, durationMs = 3000L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_2, durationMs = 2000L),
+)
+
+
+val heroAnimationSequence = listOf(
+    AnimStep(drawableResId = R.drawable.mrevil_angry_1, durationMs = 3000L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_2, durationMs = 2000L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_1, durationMs = 3000L),
+    AnimStep(drawableResId = R.drawable.mrevil_angry_7, durationMs = 2000L),
+)
